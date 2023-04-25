ee7586c466f390186554c3baa13dffac0b8b1bb3
Febri Milansyah <febrimilan00@gmail.com>
2023-04-25 16:43:56 +0700

    Add Reload Commands

--- commands/fun/ping.js
+++ commands/fun/ping.js
@@ -5,6 +5,7 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
+	category: 'fun', // Folder name
 	async execute(interaction) {
 		await interaction.reply('Pong!');
 	},
--- commands/utility/user.js
+++ commands/utility/user.js
@@ -5,6 +5,7 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('user')
 		.setDescription('Provides information about the user.'),
+	category: 'utility', // Folder name
 	async execute(interaction) {
 		// interaction.user is the object representing the User who ran the command
 		// interaction.member is the GuildMember object, which represents the user in the specific guild
